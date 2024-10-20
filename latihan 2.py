3d29c86b05a7b0561d58bc0917c56df9779f6c09
dimas112p <dimasprasetyo6930@gmail.com>
2024-10-20 23:53:20 +0800

    Delete latihan 2.py

--- latihan 2.py
+++
@@ -1,9 +0,0 @@
-#input bilangan dari pengguna 
-bilangan = float(input("masukkan sebuah bilangan: "))
-#struktur percabangan
-if bilangan > 0: 
-    print("bilangan tersebut adalah positif.")
-elif bilangan < 0: 
-    print("bilangan tersebut adalah negatif.")
-else:
-    print("bilangan tersebut adalah nol.")
